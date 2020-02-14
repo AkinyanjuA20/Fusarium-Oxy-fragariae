70854becae338e41ab109485ce8fe9e1840ad823
akinya <akinya@em-hpc01.emr.ac.uk>
2020-02-14 14:04:27 +0000

    created the main pipeline script

---
+++ scripts/main_pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ProgDir=/home/akinya/git_repos/tools/seq_tools/assemblers/assembly_qc/quast
+
+for Assembly in $(ls assembly/spades/*/*/filtered_contigs/contigs_min_500bp.fasta)
+do
+  Strain=$(echo $Assembly | rev | cut -f3 -d '/' | rev)
+  Organism=$(echo $Assembly | rev | cut -f4 -d '/' | rev)
+  # OutDir=assembly/spades/$Organism/$Strain
+  OutDir=$(dirname $Assembly)
+
+  sbatch $ProgDir/sub_quast.sh $Assembly $OutDir
+done

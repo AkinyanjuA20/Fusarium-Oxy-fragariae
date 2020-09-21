8e09f9cd44cd3010691931fb3a8043d040a84f93
AkinyanjuA <59013286+AkinyanjuA@users.noreply.github.com>
2020-09-21 09:00:32 +0100

    Updates to MosaicFLye after emailing designer

--- Genome_assembly/mosaicFlye.sh
+++ Genome_assembly/mosaicFlye.sh
@@ -10,4 +10,14 @@
 # mosaic --reads <file> -o <dir> (--genome-size <int> | --flye-dir <dir> | --contigs <contigs>) [--threads <int>]
 # removed --threads
 
-python2 /home/akinya/git_repos/assembly_fusarium_ex/mosaic/mosaic --reads assembly/flye/F.oxysporum_fsp_lactucae/race_1/FAL_trim.fastq.gz -o assembly/mosaicFLye/flye (--genome-size 60000000 | --flye-dir /home/akinya/miniconda3/envs/olc_assemblers/bin/ | --contigs assembly/flye/F.oxysporum_fsp_lactucae/race_1/assembly.fasta)
+# Examples of how to run mosaicFLye correctly - example 2 is best otherwise it reruns flye
+# python2 /path/to/mosaic --reads path/to/reads/xyz.fastq.gz -o /path/to/output 60000000
+# python2 /path/to/mosaic --reads path/to/reads/xyz.fastq.gz -o /path/to/output --flye-dir path/to/flye --threads 8
+# python2 /path/to/mosaic --reads path/to/reads/xyz.fastq.gz -o /path/to/output --genome-size 60000000 --flye-dir path/to/flye --contigs path/to/contigs --threads 8
+
+python2 /home/akinya/git_repos/assembly_fusarium_ex/mosaic/ --reads assembly/flye/F.oxysporum_fsp_lactucae/race_1/FAL_trim.fastq.gz -o assembly/mosaicFLye/flye --flye-dir /home/akinya/miniconda3/envs/olc_assemblers/bin/ --threads 8
+
+# Examples of how to run mosaicFLye correctly
+# python2 /path/to/mosaic --reads path/to/reads/xyz.fastq.gz -o /path/to/output 60000000
+# python2 /path/to/mosaic --reads path/to/reads/xyz.fastq.gz -o /path/to/output --flye-dir path/to/flye --threads 8
+# python2 /path/to/mosaic --reads path/to/reads/xyz.fastq.gz -o /path/to/output --genome-size 60000000 --flye-dir path/to/flye --contigs path/to/contigs --threads 8

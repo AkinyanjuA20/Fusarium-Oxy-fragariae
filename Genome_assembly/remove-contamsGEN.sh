077fddd52ebc965f57551f27d5fc67f824ecc652
AkinyanjuA <59013286+AkinyanjuA@users.noreply.github.com>
2020-09-29 15:21:17 +0100

    Remove contaminants but Antonio style

---
+++ Genome_assembly/remove-contamsGEN.sh
@@ -0,0 +1,10 @@
+# Rename contigs for genome
+# If split or remove contigs is needed, provide FCSreport file by NCBI.
+
+ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Assembly_qc
+    touch tmp.txt
+    for Assembly in $(ls assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/racon_10/race_1_smartdenovo_racon_round_1.fasta); do
+        OutDir=$(dirname $Assembly)
+        $ProgDir/remove_contaminants.py --inp $Assembly --out $OutDir/"GiveGenomeName"_renamed.fasta --coord_file tmp.txt > $OutDir/log.txt
+    done
+    rm tmp.txt

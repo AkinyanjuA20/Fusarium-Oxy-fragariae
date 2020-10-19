e7f7034a98531e38d3f9c3840531f3a39cd2142b
AkinyanjuA <59013286+AkinyanjuA@users.noreply.github.com>
2020-10-19 16:18:12 +0100

    Slight directory change

--- gene_pred_Scripts/busco_s11.sh
+++ gene_pred_Scripts/busco_s11.sh
@@ -5,12 +5,12 @@
 #Run in conda env - BUSCOenv
 #Ran on genome(softmasked) and gene models (final_genes_appended_renamed.gene.fasta)
 
-for Assembly in $(ls assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/racon_10/race_1_smartdenovo_racon_round_6.fasta); do
-Strain=$(echo $Assembly| rev | cut -d '/' -f3 | rev)
-Organism=$(echo $Assembly | rev | cut -d '/' -f4 | rev)
+for Assembly in $(ls assembly/flye/F.oxysporum_fsp_lactucae/race_1/flye_raw/racon_10/assembly_racon_round_1.fasta); do
+Strain=$(echo $Assembly| rev | cut -d '/' -f4 | rev)
+Organism=$(echo $Assembly | rev | cut -d '/' -f5 | rev)
 echo "$Organism - $Strain"
 ProgDir=~/git_repos/fusarium_ex_strawberry/ProgScripts
 BuscoDB=$(ls -d /projects/dbBusco/sordariomycetes_odb10)
-OutDir=$(dirname $Assembly)/busco_sordariomycetes_obd10/race_1_smartdenovo_racon_round_6
+OutDir=$(dirname $Assembly)/busco_sordariomycetes_obd10/round_1
 sbatch $ProgDir/busco.sh $Assembly $BuscoDB $OutDir
 done

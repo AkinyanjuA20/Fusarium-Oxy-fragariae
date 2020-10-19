6097bfe5e381410105e8429512cdff9101aa2446
AkinyanjuA <59013286+AkinyanjuA@users.noreply.github.com>
2020-10-19 15:57:12 +0100

    Slight changes to remember the order

--- Genome_assembly/medaka_qc.sh
+++ Genome_assembly/medaka_qc.sh
@@ -4,7 +4,18 @@
 # This task is performed using neural networks applied from a pileup of individual sequencing reads against a draft assembly.
 # It outperforms graph-based methods operating on basecalled data, and can be competitive with state-of-the-art signal-based methods, whilst being much faster.
 
-for Assembly in $(ls assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/racon_10/*_renamed.fasta); do
+# Rename contigs for genome
+# If split or remove contigs is needed, provide FCSreport file by NCBI.
+
+# ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Assembly_qc
+#    touch tmp.txt
+#    for Assembly in $(ls assembly/flye/F.oxysporum_fsp_lactucae/race_1/flye_raw/racon_10/assembly_racon_round_4.fasta); do
+#        OutDir=$(dirname $Assembly)
+#        $ProgDir/remove_contaminants.py --inp $Assembly --out $OutDir/assembly_r4_renamed_FR.fasta --coord_file tmp.txt > $OutDir/log.txt
+#    done
+#    rm tmp.txt
+
+for Assembly in $(ls assembly/flye/F.oxysporum_fsp_lactucae/race_1/flye_raw/racon_10/assembly_r4_renamed_FR.fasta); do
   ReadsFq=$(ls assembly/flye/F.oxysporum_fsp_lactucae/race_1/FAL_trim.fastq.gz)
   OutDir=$(dirname $Assembly)/medaka
   ProgDir=/home/gomeza/git_repos/scripts/bioinformatics_tools/Genome_assemblers
--- R_scripts/Convert-salmon2transgene.sh
+++ R_scripts/Convert-salmon2transgene.sh
@@ -3,7 +3,7 @@
 
 mkdir -p RNAseq_analysis/salmon/F.oxysporum_fsp_fragariae/DSA14_003/DeSeq2
 #This command creates a two column file with transcript_id and gene_id.
-for File in $(ls RNAseq_analysis/salmon/F.oxysporum_fsp_fragariae/DSA14_003/*/quant.sf | head -n1); do
+for File in $(ls RNAseq_analysis/salmon/F.oxysporum_fsp_fragariae/DSA14_003/DeSeq2/*/quant.sf | head -n1); do
   cat $File | awk -F"\t" '{c=$1;sub(".t.*","",$1);print c,$1}' OFS="\t" > RNAseq_analysis/salmon/F.oxysporum_fsp_fragariae/DSA14_003/DeSeq2/trans2gene.txt
 done
 
@@ -15,9 +15,9 @@ done
 # Put files in a convenient location for DeSeq.
 # Analysis was not performed on Apple control samples.
 
-for File in $(ls RNAseq_analysis/salmon_vAG/F.venenatum/WT_minion/*/quant.sf); do
-  Prefix=$(echo $File | cut -f5 -d '/' --output-delimiter '_')
-  mkdir -p RNAseq_analysis/salmon_vAG/F.venenatum/WT_minion/DeSeq2/$Prefix
-  cp $PWD/$File RNAseq_analysis/salmon_vAG/F.venenatum/WT_minion/DeSeq2/$Prefix/quant.sf
+for File in $(ls RNAseq_analysis/salmon/F.oxysporum_fsp_fragariae/DSA14_003/DeSeq2/*/quant.sf); do
+  Prefix=$(echo $File | cut -f6 -d '/' --output-delimiter '_')
+  mkdir -p RNAseq_analysis/salmon/F.oxysporum_fsp_fragariae/DSA14_003/DeSeq2/$Prefix
+  cp $PWD/$File RNAseq_analysis/salmon/F.oxysporum_fsp_fragariae/DSA14_003/DeSeq2/$Prefix/quant.sf
   # rm alignment/salmon/DeSeq2/$Prefix/quant.sf
 done

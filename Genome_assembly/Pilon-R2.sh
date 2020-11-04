c7f9cb69c990f1cc8e66bd593b77132ee03f4c83
AkinyanjuA <59013286+AkinyanjuA@users.noreply.github.com>
2020-11-04 14:44:28 +0000

    Program script had errors. this should be okay to work

--- Genome_assembly/Pilon-R2.sh
+++ Genome_assembly/Pilon-R2.sh
@@ -1,7 +1,13 @@
+# Aligning illumina reads against pilon data
+# Run in conda env
+# Make sure script is executable
+# chmod u+x ./sub_pilon.sh
+
 for Assembly in $(ls assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/Pilon_SDen/race_1_smartdenovo_racon_round_10_renamed.fasta); do
-Reads=$(pilon/bwa_mapping.SDEN.sorted.bam)
-OutDir= assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/Pilon_SDen/
+  FileF=$(ls ../oldhome/groups/harrisonlab/project_files/fusarium/raw_dna/paired/F.oxysporum_fsp_lactucae/AJ520/F/AJ520_S2_L001_R1_001.fastq.gz)
+  FileR=$(ls ../oldhome/groups/harrisonlab/project_files/fusarium/raw_dna/paired/F.oxysporum_fsp_lactucae/AJ520/R/AJ520_S2_L001_R2_001.fastq.gz)
+OutDir=assembly/SMARTdenovo/F.oxysporum_fsp_lactucae/race_1/Pilon_SDen/
 Iterations=10
 ProgDir=/home/akinya/git_repos/fusarium_ex_strawberry/ProgScripts
-sbatch $ProgDir/pilon_longread.sh $Assembly $BAM $OutDir $Iterations
+sbatch $ProgDir/sub_pilon.sh $Assembly $FileF $FileR $OutDir $Iterations
 done
